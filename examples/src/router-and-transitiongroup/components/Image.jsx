3fd43f9bdb7510ed9f86178182b5b2d955a9366d
Onno Visser <visser.onno@gmail.com>
2018-08-31 17:20:55 +0200

    Remove unfinished examples

--- examples/src/router-and-transitiongroup/components/Image.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'react-emotion';
-
-const Image = styled.div`
-  width: 100%;
-  height: 100%;
-  background: no-repeat center/cover url(${p => p.src});
-`;
-
-export default Image;
